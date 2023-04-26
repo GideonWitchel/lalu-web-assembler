45f6109e7cb7abb27d7ae294cb543a3da67c6317
Gideon Witchel <94641536+GideonWitchel@users.noreply.github.com>
2023-04-26 15:51:40 -0500

    Server side assembly
    And ability to copy output

--- static/javascript/webassembler.js
+++ static/javascript/webassembler.js
@@ -2,11 +2,20 @@ window.addEventListener("load", function(){
     // File downloads
     let inputText = document.getElementById('inputArea');
     let outputText = document.getElementById('outputArea');
+    let outputCode = document.getElementById('machineOutputArea')
     document.getElementById('downloadMachine').addEventListener("click", function(e) {
-        e.target.href = 'data:text/plain;charset=utf-11,' + encodeURIComponent(outputText.value);
+        e.target.href = 'data:text/plain;charset=utf-11,' + encodeURIComponent("Original Program\n") + encodeURIComponent(outputText.value) + encodeURIComponent("\nProgram Memory (copy & paste this into your program memory)\n\n") + encodeURIComponent(outputCode.value);
     });
     document.getElementById('downloadAssembly').addEventListener("click", function(e) {
         e.target.href = 'data:text/plain;charset=utf-11,' + encodeURIComponent(inputText.value);
     });
+    document.getElementById('copy').addEventListener("click", async function() {
+        outputCode.select();
+        outputCode.setSelectionRange(0, 99999); // For mobile devices
+        await navigator.clipboard.writeText(outputCode.value);
+        // Clear highlighting
+        window.getSelection().removeAllRanges();
+        outputCode.selection.empty();
+        });
 });
 
