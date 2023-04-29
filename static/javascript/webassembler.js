ba51ec393bb20a1592eec765a9366ebae5a3b60e
Gideon Witchel <94641536+GideonWitchel@users.noreply.github.com>
2023-04-28 19:32:35 -0500

    File drop, formatting, polish

--- static/javascript/webassembler.js
+++ static/javascript/webassembler.js
@@ -3,12 +3,15 @@ window.addEventListener("load", function(){
     let inputText = document.getElementById('inputArea');
     let outputText = document.getElementById('outputArea');
     let outputCode = document.getElementById('machineOutputArea')
+
     document.getElementById('downloadMachine').addEventListener("click", function(e) {
         e.target.href = 'data:text/plain;charset=utf-11,' + encodeURIComponent("Original Program\n") + encodeURIComponent(outputText.value) + encodeURIComponent("\nProgram Memory (copy & paste this into your program memory)\n\n") + encodeURIComponent(outputCode.value);
     });
+
     document.getElementById('downloadAssembly').addEventListener("click", function(e) {
         e.target.href = 'data:text/plain;charset=utf-11,' + encodeURIComponent(inputText.value);
     });
+
     document.getElementById('copy').addEventListener("click", async function() {
         outputCode.select();
         outputCode.setSelectionRange(0, 99999); // For mobile devices
@@ -17,5 +20,20 @@ window.addEventListener("load", function(){
         window.getSelection().removeAllRanges();
         outputCode.selection.empty();
         });
-});
 
+    window.addEventListener("drop", function(e) {
+        e.preventDefault()
+        let files = e.dataTransfer.files
+        let reader = new FileReader();
+
+        reader.onload = function () {
+            inputText.innerHTML = reader.result.replace(/\r\n|\n/, "\n");
+        };
+
+        if(files[0]) {
+            // This does not return the text. It just starts reading.
+            // The onload handler is triggered when it is done and the result is available.
+            reader.readAsText(files[0]);
+        }
+    });
+});
