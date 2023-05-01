7381a3cd2778991f0bc987eb53c6299c23d46b81
Gideon Witchel <94641536+GideonWitchel@users.noreply.github.com>
2023-05-01 09:48:33 -0500

    Auto copy cookie

--- static/javascript/webassembler.js
+++ static/javascript/webassembler.js
@@ -1,4 +1,6 @@
-window.addEventListener("load", function(){
+window.addEventListener("load", async function(){
+    await initAutoCopy()
+
     // File downloads
     let inputText = document.getElementById('inputArea');
     let outputText = document.getElementById('outputArea');
@@ -13,13 +15,8 @@ window.addEventListener("load", function(){
     });
 
     document.getElementById('copy').addEventListener("click", async function() {
-        outputCode.select();
-        outputCode.setSelectionRange(0, 99999); // For mobile devices
-        await navigator.clipboard.writeText(outputCode.value);
-        // Clear highlighting
-        window.getSelection().removeAllRanges();
-        outputCode.selection.empty();
-        });
+        await copyItem(outputCode)
+    });
 
     window.addEventListener("drop", function(e) {
         e.preventDefault()
@@ -38,4 +35,70 @@ window.addEventListener("load", function(){
             reader.readAsText(files[0]);
         }
     });
+
+    // Indicate that the code is stale
+    $("#inputArea").change(function() {
+        outputText.style.backgroundColor = "dimgray"
+        outputCode.style.backgroundColor = "dimgray"
+        outputText.style.color = "white"
+        outputCode.style.color = "white"
+    });
+
+    $("#auto-copy").change(function() {
+        if (this.checked){
+            enableAutoCopy()
+        }
+        else {
+            disableAutoCopy()
+        }
+    })
 });
+
+async function copyItem(toCopy) {
+    toCopy.select();
+    toCopy.setSelectionRange(0, 99999); // For mobile devices
+    await navigator.clipboard.writeText(toCopy.value);
+    // Clear highlighting
+    window.getSelection().removeAllRanges();
+}
+
+function enableAutoCopy(){
+    // Expire time is near max 32 bit int
+    document.cookie = "autocopy=1; expires=Fri, 1 Jan 2038 00:00:00 UTC; path=/"
+}
+
+function disableAutoCopy() {
+    // Expire time is near max 32 bit int
+    document.cookie = "autocopy=0; expires=Fri, 1 Jan 2038 00:00:00 UTC; path=/"
+}
+
+async function initAutoCopy() {
+    // Make sure the cookie is valid
+    let currCookie = document.cookie.split(';');
+    let autoCopy = $("#auto-copy")
+
+    // Find cookie
+    for(let i= 0; i < currCookie.length; i++) {
+        let cookie = currCookie[i];
+        while (cookie.length > 0) {
+            if (cookie.indexOf("autocopy") === 0) {
+                // Execute
+
+                currCookie = cookie.substring("autocopy".length, cookie.length);
+                // If cookie is not 1, it's disabled
+                if (currCookie[1] !== "1"){
+                    autoCopy.prop("checked", false)
+                    return
+                }
+
+                // Otherwise the cookie is 1
+                // Restore switch and copy
+                autoCopy.prop("checked", true)
+                await copyItem(document.getElementById("machineOutputArea"))
+                return
+            }
+            cookie = cookie.substring(1, cookie.length);
+        }
+    }
+    autoCopy.prop("checked", false)
+}
